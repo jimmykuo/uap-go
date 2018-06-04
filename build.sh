00c3ea6e50a8211d12f1287bef928309097377e4
Jimmy Kuo <kuo.yangyang@gmail.com>
2018-06-04 19:10:53 +0800

    Add custom device to yaml.go

--- build.sh
+++ build.sh
@@ -3,7 +3,19 @@
 git submodule update
 
 # Strip out empty lines and comments for conciseness:
-yaml=`cat uap-core/regexes.yaml | sed '/\s*#/d' | sed '/^\s*$/d'`
+# yaml=`cat uap-core/regexes.yaml | sed '/\s*#/d' | sed '/^\s*$/d'`
+
+# insert constom.yaml
+src="uap-core/regexes.yaml"
+custom="custom.yaml"
+match=`grep -n '# Android General Device Matching' $src|cut -d':' -f1`
+# echo $match
+insert_point=`expr $match - 2`
+echo "insert_point:$insert_point"
+
+# Strip out empty lines and comments
+# SonyEricsson is equal to Sony
+yaml=`sed "$insert_point r $custom" $src | sed '/\s*#/d' | sed '/^\s*$/d' | sed 's/brand_replacement: '\''SonyEricsson'\''/brand_replacement: '\''Sony'\''/g'`
 
 # Build and format a Go file including our sources:
 echo "package uaparser
